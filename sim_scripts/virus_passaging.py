bcfaa891bf80cd95e6cb9a580cb90e3331fb42fc
bonse <eva.bons@env.ethz.ch>
2017-12-04 16:47:49 +0100

    changed virus passaging to a class

--- sim_scripts/virus_passaging.py
+++ sim_scripts/virus_passaging.py
@@ -9,28 +9,47 @@ import optparse
 import seq_sim as sim
 
 
-def passaging(settings,n_gen=50,initial_size=1,max_size=1e5,transfer_size=0.005,
-              transfer_time=2):
+# def passaging(settings,n_gen=50,initial_size=1,max_size=1e5,transfer_size=0.005,
+#               transfer_time=2):
+#
+#     phage_sim = sim.Simulation(simulation_settings=settings,
+#                                n_seq_init=initial_size,
+#                                max_pop = max_size)
+#
+#
+#
+#     for gen in range(1,n_gen+1):
+#         if gen % transfer_time == 0:
+#             transferred = int(transfer_size*phage_sim.current_gen.n_seq)
+#             if transferred > 1:
+#                 phage_sim.settings['max_pop'] = transferred
+#             else:
+#                 phage_sim.settings['max_pop'] = 2
+#             phage_sim.new_generation()
+#             phage_sim.settings['max_pop'] = max_size
+#         else:
+#             phage_sim.new_generation()
+#
+#     return phage_sim
 
-    phage_sim = sim.Simulation(simulation_settings=settings,
-                               n_seq_init=initial_size,
-                               max_pop = max_size)
+class passaging(sim.Simulation):
+    def __init__(self,simulation_settings,initial_size,max_size,
+                            transfer_prop,transfer_time, **kwargs):
+        sim.Simulation.__init__(self,simulation_settings,n_seq_init=initial_size,**kwargs)
+        self.initial_size = initial_size
+        self.max_size = max_size
+        self.transfer_prop = transfer_prop
+        self.transfer_time = transfer_time
 
 
+    def passage(self,n_passage):
+        for i in range(n_passage):
+            for j in range(self.transfer_time-1):
+                self.new_generation()
+            self.settings['max_pop'] = max(2,self.transfer_prop*self.current_gen.n_seq)
+            self.new_generation()
+            self.settings['max_pop'] = self.max_size
 
-    for gen in range(1,n_gen+1):
-        if gen % transfer_time == 0:
-            transferred = int(transfer_size*phage_sim.current_gen.n_seq)
-            if transferred > 1:
-                phage_sim.settings['max_pop'] = transferred
-            else:
-                phage_sim.settings['max_pop'] = 2
-            phage_sim.new_generation()
-            phage_sim.settings['max_pop'] = max_size
-        else:
-            phage_sim.new_generation()
-
-    return phage_sim
 
 
 if __name__ ==  '__main__':
@@ -59,6 +78,8 @@ if __name__ ==  '__main__':
 
     (options, args) = parser.parse_args()
 
+    if options.transfer_time is None:
+        options.transfer_time = 2
 
     if options.n_gen is None and options.n_pass is None:
         parser.error('either number of generations (-g) or number of passages (-p) is required')
@@ -71,16 +92,16 @@ if __name__ ==  '__main__':
         options.settings = 'phix174'
     if options.max_size is None:
         options.max_size = 1e5
-    if options.transfer_time is None:
-        options.transfer_time = 2
+
     if options.transfer_prop is None:
-        options.transfer_prop = 0.05
+        options.transfer_prop = 0.005
 
     if options.initial_size is None:
-        options.initial_size = 1
+        options.initial_size = 1e5
 
-    phage_sim = passaging(options.settings,int(options.n_gen),
-                          options.initial_size,options.max_size,
+    phage_sim = passaging(options.settings,options.initial_size,options.max_size,
                           float(options.transfer_prop),options.transfer_time)
+
+    phage_sim.passage(100)
 
     print phage_sim
